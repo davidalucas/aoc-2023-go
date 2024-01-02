834be3ca2d7ff90ace434282af92592782432231
David Lucas <davidalucas92@gmail.com>
2024-01-02 11:47:38 -0500

    Implements the GetPower function for the CubeGame struct
    This is the one which works for an individual CubeGame item

--- day2/day2_test.go
+++ day2/day2_test.go
@@ -116,3 +116,24 @@ func Test_SumPossibleGames_With_Real_Data(t *testing.T) {
 		t.Errorf("Expected %v, got %v", expected, actual)
 	}
 }
+
+func Test_GetPower_With_Sample_Data(t *testing.T) {
+	data := []string{
+		"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+		"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+		"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+		"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+		"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+	}
+	expected := []int{48, 12, 1560, 630, 36}
+
+	results := make([]int, len(data))
+	for i, v := range data {
+		game, _ := MakeCubeGame(v)
+		results[i] = game.GetPower()
+	}
+
+	if !reflect.DeepEqual(results, expected) {
+		t.Errorf("Expected %+v, got %+v", expected, results)
+	}
+}
