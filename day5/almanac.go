78f639a24f2767d4dbc9bb52697667593b1bd3e8
David Lucas <davidalucas92@gmail.com>
2024-01-06 17:55:53 -0500

    Adds in the initial data and structs

---
+++ day5/almanac.go
@@ -0,0 +1,6 @@
+package day5
+
+type Almanac struct {
+	Seeds          []int64
+	MapCollections [][]int64
+}
---
+++ day5/almanacMap.go
@@ -0,0 +1,7 @@
+package day5
+
+type AlmanacMap struct {
+	Source      int64
+	Destination int64
+	Range       int64
+}
