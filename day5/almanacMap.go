2bb40e833bbc9cc77dc168c10dc37992b687b5cc
David Lucas <davidalucas92@gmail.com>
2024-01-06 18:15:18 -0500

    Implements the MakeAlmanacMap function
    Parses string data, creating an AlmanacMap object

--- day5/almanacMap.go
+++ day5/almanacMap.go
@@ -1,7 +1,35 @@
 package day5
 
+import (
+	"strconv"
+	"strings"
+)
+
 type AlmanacMap struct {
 	Source      int64
 	Destination int64
 	Range       int64
+}
+
+// MakeAlmanacMap constructs a new AlmanacMap object using the provided string
+// data.
+func MakeAlmanacMap(data string) (*AlmanacMap, error) {
+	splitStr := strings.Split(data, " ")
+	src, err := strconv.ParseInt(splitStr[1], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	dest, err := strconv.ParseInt(splitStr[0], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	rg, err := strconv.ParseInt(splitStr[2], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	return &AlmanacMap{
+		Source:      src,
+		Destination: dest,
+		Range:       rg,
+	}, nil
 }
