4c9d98dd6a248830920d635ce1dbacd350c3bdce
David Lucas <davidalucas92@gmail.com>
2024-01-07 14:09:17 -0500

    Adds the SelectCorrespondingMap function
    Used by the part 2 algorithm to figure out which map in the collection is the "critical" map

--- day5/mapCollection.go
+++ day5/mapCollection.go
@@ -24,3 +24,20 @@ func MakeMapCollection(dataQueue *list.List) ([]AlmanacMap, error) {
 	)
 	return mapCollection, nil
 }
+
+// SelectCorrespondingMap finds the map which corresponds to the provided src value.
+// If the src value comes before any of the maps, the first map will be returned. If
+// the src value falls between two maps, the map with the larger Source value will be
+// returned. If the src value comes after all of the maps, then the last map will be
+// returned.
+//
+// Note that the maps must be sorted in ascending order by their Source values in order
+// for this method to work correctly.
+func SelectCorrespondingMap(mapCol []AlmanacMap, src int64) AlmanacMap {
+	for _, alMap := range mapCol {
+		if src >= alMap.Source {
+			return alMap
+		}
+	}
+	return mapCol[len(mapCol)-1]
+}
