fe11e535550b7bef621e3de8257ed01950fb2dcc
David Lucas <davidalucas92@gmail.com>
2024-01-10 17:43:13 -0500

    Adds the Hand type and parser
    Includes the function for calculating the score

--- day7/camelPoker.go
+++ day7/camelPoker.go
@@ -1,1 +1,45 @@
 package day7
+
+import (
+	"math"
+	"strconv"
+	"strings"
+)
+
+type Hand struct {
+	Cards string
+	Bid   int
+	Score int
+}
+
+// MakeHand parses a raw data string, constructing the resulting
+// Hand object.
+func MakeHand(rawData string) (*Hand, error) {
+	splitData := strings.Split(rawData, " ")
+	cards := splitData[0]
+	bid, err := strconv.ParseInt(splitData[1], 10, 16)
+	if err != nil {
+		return nil, err
+	}
+	score := calcCardsScore(cards)
+
+	return &Hand{Cards: cards, Bid: int(bid), Score: score}, nil
+}
+
+// CalcCardsScore calculates the base score for a set of cards.
+func calcCardsScore(cards string) int {
+	cardMap := make(map[byte]uint8)
+	for i := 0; i < len(cards); i++ {
+		_, ok := cardMap[cards[i]]
+		if !ok {
+			cardMap[cards[i]] = 1
+			continue
+		}
+		cardMap[cards[i]]++
+	}
+	score := 0
+	for _, v := range cardMap {
+		score += int(math.Pow(float64(v), 2))
+	}
+	return score
+}
--- day7/camelPoker_test.go
+++ day7/camelPoker_test.go
@@ -1,1 +1,18 @@
 package day7
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_MakeHand_Returns_Expected_Hand(t *testing.T) {
+	expected := &Hand{Cards: "32T3K", Bid: 765, Score: 7}
+	actual, err := MakeHand("32T3K 765")
+
+	if err != nil {
+		t.Errorf("Test failed with the following error: %v", err)
+	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+}
